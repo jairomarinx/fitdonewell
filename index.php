8dfbb14564b63f0bce100d7130a919d131d1125a
PMediaJairo <jairo@profitablemedia.com>
2025-04-06 08:03:55 -0500

    6  de abril fixes

--- index.php
+++ index.php
@@ -399,75 +399,7 @@
     <!-- Facts End -->
 
 
-    <!-- Team Start -->
-    <div class="container-fluid p-5">
-        <div class="mb-5 text-center">
-            <h5 class="text-primary text-uppercase"></h5>
-            <h1 class="display-3 text-uppercase mb-0">The Team</h1>
-        </div>
-        <div class="row g-5">
-        <div class="col-lg-4 col-md-6">
-                <div class="team-item position-relative">
-                    <div class="position-relative overflow-hidden rounded">
-                        <img class="img-fluid w-100" src="img/team-laly-brad.jpg" alt="">
-                        <div class="team-overlay">
-                            <div class="d-flex align-items-center justify-content-start">
-                                <a class="btn btn-light btn-square rounded-circle mx-1" href="#"><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-light btn-square rounded-circle mx-1" href="#"><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-light btn-square rounded-circle mx-1" href="#"><i class="fab fa-linkedin-in"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="position-absolute start-0 bottom-0 w-100 rounded-bottom text-center p-4" style="background: rgba(34, 36, 41, .9);">
-                        <h5 class="text-uppercase text-light">Laly Pena & Brad Peterson</h5>
-                        <p class="text-uppercase text-secondary m-0">Founders</p>
-                    </div>
-                </div>
-            </div> 
 
-            <div class="col-lg-4 col-md-6">
-                <div class="team-item position-relative">
-                    <div class="position-relative overflow-hidden rounded">
-                        <img class="img-fluid w-100" src="img/brad-team1.jpg" alt="">
-                        <div class="team-overlay">
-                            <div class="d-flex align-items-center justify-content-start">
-                                <a class="btn btn-light btn-square rounded-circle mx-1" href="#"><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-light btn-square rounded-circle mx-1" href="#"><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-light btn-square rounded-circle mx-1" href="#"><i class="fab fa-linkedin-in"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="position-absolute start-0 bottom-0 w-100 rounded-bottom text-center p-4" style="background: rgba(34, 36, 41, .9);">
-                        <h5 class="text-uppercase text-light">Brad Peterson</h5>
-                        <p class="text-uppercase text-secondary m-0">Coach</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="team-item position-relative">
-                    <div class="position-relative overflow-hidden rounded">
-                        <img class="img-fluid w-100" src="img/laly-team1.jpg" alt="">
-                        <div class="team-overlay">
-                            <div class="d-flex align-items-center justify-content-start">
-                                <a class="btn btn-light btn-square rounded-circle mx-1" href="#"><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-light btn-square rounded-circle mx-1" href="#"><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-light btn-square rounded-circle mx-1" href="#"><i class="fab fa-linkedin-in"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="position-absolute start-0 bottom-0 w-100 rounded-bottom text-center p-4" style="background: rgba(34, 36, 41, .9);">
-                        <h5 class="text-uppercase text-light">Laly Peña</h5>
-                        <p class="text-uppercase text-secondary m-0">Coach</p>
-                    </div>
-                </div>
-            </div>
-
-           
-
-        </div>
-    </div>
-    <!-- Team End -->
-    
 
     <!-- Testimonial Start -->
     <div class="container-fluid p-0 my-5">
@@ -1010,7 +942,75 @@
     </div>
     <!-- Class Timetable Start -->
        
+    <!-- Team Start -->
+    <div class="container-fluid p-5">
+        <div class="mb-5 text-center">
+            <h5 class="text-primary text-uppercase"></h5>
+            <h1 class="display-3 text-uppercase mb-0">The Team</h1>
+        </div>
+        <div class="row g-5">
+        <div class="col-lg-4 col-md-6">
+                <div class="team-item position-relative">
+                    <div class="position-relative overflow-hidden rounded">
+                        <img class="img-fluid w-100" src="img/team-laly-brad.jpg" alt="">
+                        <div class="team-overlay">
+                            <div class="d-flex align-items-center justify-content-start">
+                                <a class="btn btn-light btn-square rounded-circle mx-1" href="https://web.facebook.com/laly.pena.54"><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-light btn-square rounded-circle mx-1" href="https://www.tiktok.com/@fit.done.well"><i class="fab fa-tiktok"></i></a>
+                                <a class="btn btn-light btn-square rounded-circle mx-1" href="https://www.instagram.com/fit.done.well/?locale=pt_BR&hl=am-et"><i class="fab fa-instagram"></i></a>
+                                <a class="btn btn-light btn-square rounded-circle mx-1" href="https://www.youtube.com/lalytawellness"><i class="fab fa-youtube"></i></a>
+                                <a class="btn btn-light btn-square rounded-circle mx-1" href="https://br.pinterest.com/lalytawellness/"><i class="fab fa-pinterest"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="position-absolute start-0 bottom-0 w-100 rounded-bottom text-center p-4" style="background: rgba(34, 36, 41, .9);">
+                        <h5 class="text-uppercase text-primary">Laly Pena & Brad Peterson</h5>
+                        <p class="text-uppercase text-secondary m-0">Founders</p>
+                    </div>
+                </div>
+            </div> 
 
+            <div class="col-lg-4 col-md-6">
+                <div class="team-item position-relative">
+                    <div class="position-relative overflow-hidden rounded">
+                        <a href="http://blog.fitdonewell.com/brad-peterson/"><img class="img-fluid w-100" src="img/brad-team1.jpg" alt=""></a>
+                        <div class="team-overlay">
+                            <div class="d-flex align-items-center justify-content-start">
+                                <a class="btn btn-light btn-square rounded-circle mx-1" href="http://blog.fitdonewell.com/brad-peterson/">Bio</a>
+                                <a class="btn btn-light btn-square rounded-circle mx-1" href="http://blog.fitdonewell.com/brad-peterson/">Certs</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="position-absolute start-0 bottom-0 w-100 rounded-bottom text-center p-4" style="background: rgba(34, 36, 41, .9);">
+                        <h5 class="text-uppercase text-primary"><a href="http://blog.fitdonewell.com/brad-peterson/"> Brad Peterson</a></h5>
+                        <p class="text-uppercase text-secondary m-0">Coach</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="team-item position-relative">
+                    <div class="position-relative overflow-hidden rounded">
+                        <img class="img-fluid w-100" src="img/laly-team1.jpg" alt="">
+                        <div class="team-overlay">
+                            <div class="d-flex align-items-center justify-content-start">
+                            <a class="btn btn-light btn-square rounded-circle mx-1" href="http://blog.fitdonewell.com/laly-pena/">Bio</a>
+                            <a class="btn btn-light btn-square rounded-circle mx-1" href="http://blog.fitdonewell.com/laly-pena/">Certs</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="position-absolute start-0 bottom-0 w-100 rounded-bottom text-center p-4" style="background: rgba(34, 36, 41, .9);">
+                        <h5 class="text-uppercase text-primary"> <a href="http://blog.fitdonewell.com/laly-pena/"> Laly Peña</a></h5>
+                        <p class="text-uppercase text-secondary m-0">Coach</p>
+                    </div>
+                </div>
+            </div>
+
+           
+
+        </div>
+    </div>
+    <!-- Team End -->
+    
 
     <!-- Blog Start -->
     <div class="container-fluid p-5 d-none">
@@ -1105,7 +1105,7 @@
                             <a class="text-secondary mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
                             <a class="text-secondary mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>About Us</a>
                             <a class="text-secondary mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Class Schedule</a>
-                            <a class="text-secondary mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Our Trainers</a>
+                            <a class="text-secondary mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Our Coaches</a>
                             <a class="text-secondary mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Latest Blog</a>
                             <a class="text-secondary" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Contact Us</a>
                         </div>
